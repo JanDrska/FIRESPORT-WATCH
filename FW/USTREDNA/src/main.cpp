3db8e9fd807d2adc263068d57b59f17f2e5a8586
JanDrska <jan.drska@atlas.cz>
2022-04-03 18:48:44 +0200

    Predelany main za pomoci novych souboru - optimalizace kodu

--- FW/USTREDNA/src/main.cpp
+++ FW/USTREDNA/src/main.cpp
@@ -14,20 +14,19 @@
 
 #include "sdhTimer.h"
 #include "sdhDisplay.h"
+
 #include <LiquidCrystal_I2C.h>
 #include <PCF8574.h>
 #include <SPI.h>
 #include <RF24.h>
 #include <RF24_config.h>
 #include <nRF24L01.h>
-#include <printf.h>
 
 #define TLA 2      //tlačítko A - START
 #define TLB 3      //tlačítko B - RESET
 #define TLProg 8   //prepinac programu
 
 bool terc1, terc2, prazdno = LOW;    //terc 1 - LEVY a 2 - PRAVY
-
 
 #define ODPOCET_TERC 300000  //pozadovany cas v ms (300 000 je 5 minut)
 
@@ -36,76 +35,27 @@ TimerData timerR;
 
 DisplaySdh display;
 
-typedef struct TERC_DATA 
-{
-  long casSTART = 0;          //startovní čas
-  long casODPOCET = 0;
-  long casTERC = 0;          //průběžný čas 1. terče
-  long casTERC_end = 0;       //konečný čas 1. terče
-  long casTERC_M = 0;         //konečný čas 1. terče - minuty
-  long casTERC_S = 0;         //konečný čas 1. terče - sekundy
-  long casTERC_ms = 0;        //konečný čas 1. terče - ms
-};
-
-struct TERC_DATA TERC_L;
-struct TERC_DATA TERC_P;
-
 bool init_delivery_confirm_bit = LOW;
 bool READY = LOW;
 const char text1[] = "INIT";
 const char text2[] = "RESET";
-//const char text3[] = "IS_READY";
-//const char text4 [32] = "";
 char text [32] = "";
-
 
 int STOP1 = 0;
 int STOP2 = 0;
 int program =0;
-
-// dp ABCGFED
-
-byte cislice[10] = 
-{B01110111,B00110000, // 0,1
- B01101011,B01111001, // 2,3 
- B00111100,B01011101, // 4,5 
- B01011111,B01110000, // 6,7 
- B01111111,B01111101};// 8,9 
-
-byte I2C_address[8]= 
-{0x20,0x21,           //prvni a druhy modul
-0x25,0x24,            //treti a ctvrty modul
-0x22,0x23,            //paty a sesty modul
-0x26,0x27};           //sedmy a osmy modul
 
 LiquidCrystal_I2C lcd(0x3F,16,2);  
 
 RF24 radio(9, 10);  // CE, CSN
 const byte addresses [] [6] = {"00001","00002"} ; // adresa komunikatoru
 
-int rozdeleni[2]; //pole pro ulozeni rozdeleni dvoucifernych cisel pro bcd kod (promenna DaM)
-int split(int* DaM, int cas);     // funkce, ktera rozdeli dvojciferne cislo na jednotky a desitky
-int I2C_7segment_write(int val, int address); // funkce pro zapis jednoho byte na jednotku I2C
-void initialize_7segment(); // funkce na inicializaci displeje
-void begin_initialize_7segment(); // funkce na pocatecni inicializaci - pomalejsi
 void choose_program(); // funkce na vyber programu
 void start();
-
-void multiplex_7segment_R();
-void multiplex_7segment_L();
 
 void setup() 
 {
   Serial.begin(9600);
-
-  display.init();
-
-  lcd.init();  
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print(" Casomira v 2.0");
-  lcd.setCursor(0,1);
-  lcd.print("  SDH  JIZBICE");
 
   radio.begin();
   radio.openWritingPipe(addresses[0]); // 00001
@@ -120,16 +70,7 @@ void setup()
 
   Serial.println("CASOMIRA SDH JIZBICE");
   
-  begin_initialize_7segment();  //inicializace 7segmentovek
-
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(" VYBER PROGRAM");
-  lcd.setCursor(0,1);
-  lcd.print(" STOPKY/ODPOCET");
-  delay(1000);
-  lcd.clear();
-
+  display.init();  //inicializace 7segmentovek
   choose_program();
 }
 
@@ -139,7 +80,7 @@ void loop()
   {
     case 1: //######## STOPKY POZARNI SPORT ########
       
-      TERC_P.casSTART = 1;
+      timerR.casSTART = 1;
      /* if(!init_delivery_confirm_bit)
       {
         radio.stopListening();
@@ -161,8 +102,8 @@ void loop()
           lcd.print("     TERCU");
           program = 6;
         }
-        */
-     // }
+        
+     }*/
      
       if (radio.available())
       {
@@ -172,7 +113,7 @@ void loop()
         if (ZPRAVA == "PRIPRAVEN") READY = HIGH;
         
       }
-// SMAZAT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      // SMAZAT !!!!!!!!!!!!!!!!
       READY = HIGH;
 
       if((digitalRead(TLA) == LOW) && READY) //při stisknutí start...
@@ -180,95 +121,29 @@ void loop()
            start();        //skoč na start
         }
     
-        while(TERC_P.casSTART > 1)   //pokud bylo odstartováno pak
+        while(timerL.casSTART > 1)   //pokud bylo odstartováno pak
         {
 
           if(STOP1 == 0)    //průběžný čas prvního terče
           {
-            
-            TERC_L.casTERC = millis() - TERC_P.casSTART;
-            TERC_L.casTERC_M = TERC_L.casTERC / 60000;
-            TERC_L.casTERC_S = (TERC_L.casTERC % 60000)/1000;
-            TERC_L.casTERC_ms = TERC_L.casTERC % 1000;
-                
-            lcd.setCursor(0,0);               //první řádek
-            lcd.print("1. ");
-            lcd.print(TERC_L.casTERC_M);
-            Serial.print("TERC L: ");
-            Serial.print(TERC_L.casTERC_M);
-            Serial.print(" : ");     
-            lcd.print(":");
-            Serial.print(TERC_L.casTERC_S);
-            Serial.print(" : ");
-            lcd.print(TERC_L.casTERC_S);
-            lcd.print(":");
-            lcd.print(TERC_L.casTERC_ms);
-            lcd.print("   ");
-            Serial.print(TERC_L.casTERC_ms);
-            Serial.print("   ");
-    
-            multiplex_7segment_L();
-                    
+            timerL.Time();
+            timerL.sendDataSerial('L');
+            display.sendData(timerL,timerR);        
           }
               
           if(STOP2 == 0)    //průběžný čas 2.terče
           {
-            TERC_P.casTERC = millis() - TERC_P.casSTART;
-            TERC_P.casTERC_M = TERC_P.casTERC / 60000;
-            TERC_P.casTERC_S = (TERC_P.casTERC % 60000)/1000;
-            TERC_P.casTERC_ms = TERC_P.casTERC % 1000;
-            lcd.setCursor(0,1);               //druhý řádek
-            lcd.print("2. ");
-            lcd.print(TERC_P.casTERC_M);
-            //Serial.print("TERC P: ");
-            //Serial.print(TERC_P.casTERC_M);
-            //Serial.print(" : ");
-            lcd.print(":");
-            lcd.print(TERC_P.casTERC_S);
-            //Serial.print(TERC_P.casTERC_S);
-            //Serial.print(" : ");
-            lcd.print(":");
-            lcd.print(TERC_P.casTERC_ms);
-            lcd.print("   ");
-            //Serial.println(TERC_P.casTERC_ms);
-                  
-            multiplex_7segment_R();   
-    
+            timerR.Time();
+            timerR.sendDataSerial('R');
+            display.sendData(timerL,timerR);
           }
 
-    
           if(terc1 == HIGH) //konec 1.terče
           {
             Serial.println("L STOP");
             STOP1 = 1;
-            TERC_L.casTERC_end = millis() - TERC_P.casSTART;
-            TERC_L.casTERC_M = TERC_L.casTERC_end / 60000;  
-            TERC_L.casTERC_S = TERC_L.casTERC_end % 60000;  
-            TERC_L.casTERC_S = TERC_L.casTERC_S / 1000;     
-            TERC_L.casTERC_ms = TERC_L.casTERC_end % 1000;  
-    
-            multiplex_7segment_L();   //zapsani konecneho casu
-                
-            lcd.setCursor(0,0);               
-            lcd.print("1. ");
-                
-            if(TERC_L.casTERC_M < 10)      
-            {
-              lcd.print("0");     
-            }
-     
-            lcd.print(TERC_L.casTERC_M);   
-            lcd.print(":");         
-                
-            if(TERC_L.casTERC_S < 10)      
-            {
-              lcd.print("0");     
-            }
-            
-            lcd.print(TERC_L.casTERC_S);   
-            lcd.print(":");         
-            lcd.print(TERC_L.casTERC_ms);  
-
+            timerL.stopTimming();
+            display.sendData(timerL,timerR);
             terc1 = LOW;
           }
     
@@ -276,43 +151,15 @@ void loop()
           {
             Serial.println("P STOP");
             STOP2 = 1;
-            TERC_P.casTERC_end = millis() - TERC_P.casSTART;
-            TERC_P.casTERC_M = TERC_P.casTERC_end / 60000;  //výpočet minut
-            TERC_P.casTERC_S = TERC_P.casTERC_end % 60000;  //výpočet sekund
-            TERC_P.casTERC_S = TERC_P.casTERC_S / 1000;     //výpočet sekund
-            TERC_P.casTERC_ms = TERC_P.casTERC_end % 1000;  //výpočet ms
-    
-            multiplex_7segment_R();   //zapsani konecneho casu
-                
-            lcd.setCursor(0,1);               
-            lcd.print("2. ");
-                
-            if(TERC_P.casTERC_M < 10)      
-            {
-              lcd.print("0");     
-            }
-
-            lcd.print(TERC_P.casTERC_M);   
-            lcd.print(":");         
-                
-            if(TERC_P.casTERC_S < 10)      
-            {
-              lcd.print("0");     
-            }
-            
-            lcd.print(TERC_P.casTERC_S);   
-            lcd.print(":");         
-            lcd.print(TERC_P.casTERC_ms); 
+            timerR.stopTimming();
+            display.sendData(timerL,timerR);
             terc2 = LOW; 
           }
     
-    
-
           if(digitalRead(TLB) == LOW) //při stisknutí B restart
           {
-            TERC_P.casSTART = 1;
-            lcd.clear(); 
-            initialize_7segment();   //inicializace segmentovek nulami
+            timerR.casSTART = 1;
+            display.reset();   //inicializace segmentovek nulami
             choose_program();
             STOP1=0;
             STOP2=0;
@@ -342,75 +189,46 @@ void loop()
             Serial.println(terc2);
             
             radio.writeAckPayload(1, &text, sizeof(text));
-            
-          
           }
         }
      break;
 
     case 2: // ######## ODPOCET ########
              
-      TERC_P.casSTART = 1;
-      lcd.setCursor(0,0);
-      lcd.print(" Cekam na start");
-      lcd.setCursor(0,1);
-      lcd.print("    ODPOCTU");
+      timerR.casSTART = 1;
     
       if(digitalRead(TLA) == LOW) //při stisknutí start...
-        {
-           start();        //skoč na start
-           lcd.clear();
-        }
+        start();        //skoč na start
+        
    
-        while(TERC_P.casSTART > 1)   //pokud bylo odstartováno pak
-        {
+      while(timerR.casSTART > 1)   //pokud bylo odstartováno pak
+      {
                
-          TERC_L.casODPOCET =TERC_L.casSTART-millis()+ODPOCET_TERC;
-          TERC_L.casTERC_M = TERC_L.casODPOCET / 60000;  //výpočet minut
-          TERC_L.casTERC_S = TERC_L.casODPOCET % 60000;  //výpočet sekund
-          TERC_L.casTERC_S = TERC_L.casTERC_S / 1000;     //výpočet sekund
-          TERC_L.casTERC_ms = TERC_L.casODPOCET % 1000;  //výpočet ms
+        timerR.casODPOCET =timerR.casSTART-millis()+ODPOCET_TERC;
+        timerR.casTERC_M = timerR.casODPOCET / 60000;  //výpočet minut
+        timerR.casTERC_S = timerR.casODPOCET % 60000;  //výpočet sekund
+        timerR.casTERC_S = timerR.casTERC_S / 1000;     //výpočet sekund
+        timerR.casTERC_ms = timerR.casODPOCET % 1000;  //výpočet ms
 
-          lcd.setCursor(0,0);               //první řádek
-          lcd.print("    ODPOCET");
-          lcd.setCursor(4,1);
-          lcd.print(TERC_L.casTERC_M);
-          Serial.print("ODPOCET ");
-          Serial.print(TERC_L.casTERC_M);
-          Serial.print(" : ");     
-          lcd.print(":");
-          Serial.print(TERC_L.casTERC_S);
-          Serial.print(" : ");
-          lcd.print(TERC_L.casTERC_S);
-          lcd.print(":");
-          lcd.print(TERC_L.casTERC_ms);
-          lcd.print("   ");
-          Serial.print(TERC_L.casTERC_ms);
-          Serial.println("   ");
-
-          multiplex_7segment_L();
+        Serial.print("ODPOCET ");
+        timerR.sendDataSerial('R');
     
-          if((TERC_L.casTERC_M==0)&&(TERC_L.casTERC_S==0))
-          {
-            program=1;
-            TERC_L.casTERC_ms=0;
-            lcd.clear();
-            initialize_7segment();
-            break;               
-          }
-
-          if(digitalRead(TLB) == LOW) //při stisknutí B restart
-          {
-            TERC_P.casSTART = 1;
-            lcd.clear(); 
-            initialize_7segment();   //inicializace segmentovek nulami
-            break;
-          }
-
-
-
+        if((timerL.casTERC_M==0)&&(timerL.casTERC_S==0))
+        {
+          program=1;
+          timerL.casTERC_ms=0;
+          display.reset();
+          break;               
         }
-        case 6: // ERROR - RESET NUTNY
+
+        if(digitalRead(TLB) == LOW) //při stisknutí B restart
+        {
+          timerR.casSTART = 1; 
+          display.reset();
+          break;
+        }
+      }
+    case 6: // ERROR - RESET NUTNY
         break;
    break; 
   }
@@ -419,101 +237,9 @@ void loop()
 
 void start()
 {
-  TERC_P.casSTART = millis();    //uložení startovního času
-  lcd.clear();            //smaž LCD
+  timerL.startTimming();
+  timerR.startTimming();
   radio.startListening(); // zapnuti prijmu radia
-}
-
-int split(int* DaM, int cas)  //funkce na rozdeleni dvoucifernych cisel
-{
-  DaM[0]= cas % 10; //jednotky
-  if(((cas - DaM[0])/10)>=0)DaM[1] = (cas - DaM[0])/10; //desitky
-  //else DaM[1];
-
-  return 1;
-}
-
-void multiplex_7segment_L()   // program ktery zapise na segmentovky cisla ziskana konverzi pomoci funkci - LEVY TERC
-{
-//########################## MINUTY L ##############################
-
-            split(rozdeleni,TERC_L.casTERC_M);        
-
-            I2C_7segment_write(rozdeleni[0],I2C_address[0]);
-            I2C_7segment_write(rozdeleni[1],I2C_address[1]);
-            
-//########################## SEKUNDY L ##############################
-           
-            split(rozdeleni,TERC_L.casTERC_S);         
-
-            I2C_7segment_write(rozdeleni[0],I2C_address[2]);
-            I2C_7segment_write(rozdeleni[1],I2C_address[3]);        
-}
-
-void multiplex_7segment_R()   // program ktery zapise na segmentovky cisla ziskana konverzi pomoci funkci - PRAVY TERC
-{
-//########################## MINUTY R ##############################
-
-            split(rozdeleni,TERC_P.casTERC_M);            
-
-            I2C_7segment_write(rozdeleni[0],I2C_address[4]);
-            I2C_7segment_write(rozdeleni[1],I2C_address[5]);
-
-
-//########################## SEKUNDY R ##############################
-           
-            split(rozdeleni,TERC_P.casTERC_S);            
-
-            I2C_7segment_write(rozdeleni[0],I2C_address[6]);
-            I2C_7segment_write(rozdeleni[1],I2C_address[7]);
-          
-}
-
-int I2C_7segment_write(int val, int address)
-{
-  PCF8574 pcf(address);
-  pcf.write8(cislice[val]);
-  return 1;
-}
-
-void initialize_7segment()
-{
-  for(int i=0;i<=1;i++)
-  {
-      for(int i =0;i<=8;i++)
-      {
-        PCF8574 pcf(I2C_address[i]);
-        pcf.begin();
-        pcf.write8(cislice[0]);
-
-      }
-  }
-}
-
-void begin_initialize_7segment()
-{
-  for(int i=0;i<=1;i++)
-  {
-      for(int i =0;i<=8;i++)
-      {
-        PCF8574 pcf(I2C_address[i]);
-        pcf.begin();
-        pcf.write8(cislice[8]);
-        //delay(500);
-      }
-  }
-
- // delay (2000);
-
-  for(int i=0;i<=1;i++)
-  {
-      for(int i =0;i<=8;i++)
-      {
-        PCF8574 pcf(I2C_address[i]);
-        pcf.begin();
-        pcf.write8(cislice[0]);
-      }
-  }
 }
 
 void choose_program()
@@ -522,7 +248,6 @@ void choose_program()
   else
   {
     program=2;
-    start();
-    lcd.clear();    
+    start();  
   }
 }
