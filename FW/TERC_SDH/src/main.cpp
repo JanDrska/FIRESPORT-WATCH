7dadedc155afa22b2dfb0115a8d3bb9b62b96c20
Honza <xdrska04@vutbr.cz>
2023-01-16 20:02:04 +0100

    prepracovan init - pridano nucene potvrzeni zpravy za pomoci echa

--- FW/TERC_SDH/src/main.cpp
+++ FW/TERC_SDH/src/main.cpp
@@ -16,41 +16,39 @@
 
 //#### HW KONFIGURACE ####
 
-RF24 radio(9, 10);  // CE, CSN
+RF24 radio(9, 10);      // CE, CSN
 #define target1Full 8   // TERC 1 PLNY
-#define target1Empty 7   // TERC 1 PRAZDNY
+#define target1Empty 7  // TERC 1 PRAZDNY
 #define target2Full 6   // TERC 2 PLNY
 #define target2Empty 5  // TERC 2 PRAZDNY
-#define signalLight1  4   // MAJAK TERC 1
-#define signalLight2  3   // MAJAK TERC 2
+#define signalLight1  4 // MAJAK TERC 1
+#define signalLight2  3 // MAJAK TERC 2
 #define waterValve 2    // VYSTUP PRO OVLADANI waterValveU (ON/OFF)
 
 //#### GLOBALNI PROMENNE ####
 
 const byte addresses [] [6] = {"00001","00002"} ; // adresa komunikatoru
-String ZPRAVA;
+String message;
 char text [32] = "";
-const char text1[] = "LEVY";
-const char text2[] = "PRAVY";
-const char text3[] = "PRAZDNO";
-const char text4[] = "PRIPRAVEN";
+const char text1[] = "target1";
+const char text2[] = "target2";
+const char text3[] = "empty";
+const char text4[] = "ready";
+const char text5[] = "init";
 
-bool tercL, tercP = LOW;  // stavy tercu
-unsigned long cas = 0;
-long predchozi_cas = 0;
+bool target1, target2 = LOW;  // stavy tercu
+unsigned long time = 0;
+long prevTime = 0;
 bool reset = LOW;
 
-void terceInit();
-void terceInitFail();
+void initTargets();
 
 void setup()
 {
 
-
   pinMode(signalLight1,OUTPUT);
   pinMode(signalLight2,OUTPUT);
   pinMode(waterValve,OUTPUT);
-
   
   pinMode(target1Full,INPUT);
   pinMode(target1Empty,INPUT);
@@ -59,7 +57,7 @@ void setup()
 
 //#### NASTAVENI A ZAHAJENI SERIOVE KOMUNIKACE ####
   Serial.begin(9600);
-  Serial.print("\nTERC_V1");  
+  Serial.print("\nTerc V2.0 by Jan Drska");  
 
 //#### NASTAVENI A ZAHAJENI RADIOVE KOMUNIKACE ####
   radio.begin();
@@ -72,27 +70,27 @@ void setup()
   radio.startListening();  //nastaveni modulu jako vysilac
 
 // #### PRVOTNI INICIALIZACE TERCU ####
-  terceInit();
+  initTargets();
 }
 
 void loop()
 {
-  cas = millis();
+  time = millis();
   
 // #### PERIODICKA KONTROLA PRIJATYCH ZPRAV ####
 
-  if(cas - predchozi_cas > 50) // kazdych 50 ms kontrola prijatych zprav - RESET,INIT apod. 
+  if(time - prevTime > 50) // kazdych 50 ms kontrola prijatych zprav - RESET,INIT apod. 
   {
     radio.startListening();
     if(radio.available())
     {
       radio.read(&text, sizeof(text));
-      String ZPRAVA = String(text);
-      Serial.println(ZPRAVA);
-      if (ZPRAVA == "INIT") terceInit();
-      if (ZPRAVA == "RESET") reset = HIGH;
+      String message = String(text);
+      Serial.println(message);
+      if (message == "INIT") initTargets();
+      if (message == "RESET") reset = HIGH;
     }
-    predchozi_cas = cas;   
+    prevTime = time;   
   }
 
   //digitalRead(target1Full)
@@ -101,14 +99,14 @@ void loop()
 
 // #### DETEKCE NAPLNENI TERCE CISLO 1 ####
 
-  if ((digitalRead(target1Full) == 1) && !tercL)
+  if ((digitalRead(target1Full) == 1) && !target1)
   {
       //Serial.println(digitalRead(target1Full));
     radio.stopListening();
     if(radio.write(&text1, sizeof(text1)))
     {
-      Serial.write("\nLEVY");
-      tercL = HIGH;
+      Serial.write("\ntarget1");
+      target1 = HIGH;
       digitalWrite(signalLight1,HIGH);
     }
     radio.startListening(); 
@@ -116,13 +114,13 @@ void loop()
 
   // #### DETEKCE NAPLNENI TERCE CISLO 2 ####
 
-  if ((digitalRead(target2Full) == 1) && !tercP)
+  if ((digitalRead(target2Full) == 1) && !target2)
   {
     radio.stopListening();
     if(radio.write(&text2, sizeof(text2)))
     {
-      Serial.write("\nPRAVY");
-      tercP = HIGH;
+      Serial.write("\ntarget2");
+      target2 = HIGH;
       digitalWrite(signalLight2,HIGH);
     }
     radio.startListening();
@@ -130,52 +128,82 @@ void loop()
 
   // #### VYPOUSTENI ####
 
-  if ((tercL && tercP)|| reset)
+  if ((target1 && target2)|| reset)
   {
    digitalWrite(waterValve,HIGH);
-   tercP = HIGH;
-   tercL = HIGH;
+   target2 = HIGH;
+   target1 = HIGH;
   }
   //else digitalWrite(waterValve,LOW);
 
   // #### DETEKCE PRAZDNYCH TERCU ####
   
-  if ((digitalRead(target1Empty) && digitalRead(target2Empty)) && (tercL && tercP))
+  if ((digitalRead(target1Empty) && digitalRead(target2Empty)) && (target1 && target2))
   {
     reset = LOW;
     radio.stopListening();
     radio.write(&text3, sizeof(text3));
     radio.startListening();
-    Serial.write("\nPRAZDNO");
+    Serial.write("\nempty");
     digitalWrite(waterValve,LOW);
     digitalWrite(signalLight1,LOW);
     digitalWrite(signalLight2,LOW);
-    tercL = tercP = LOW;
+    target1 = target2 = LOW;
   } 
 }
 
 // #### POCATECNI INICIALIZACE TERCU , TEST waterValveU a SVETELNE SIGNALIZACE ####
 
-void terceInit()    
+void initTargets()    
 {
-  Serial.write("\nINIT");
-  Serial.write("\nSV1");
-  digitalWrite(signalLight1,HIGH);
-  digitalWrite(signalLight2,HIGH);
-  digitalWrite(waterValve,HIGH);
-  delay(2000);
-  radio.stopListening();
-  if(!radio.write(&text4, sizeof(text4))) // pokud se zprava neodesle, zacne blikat majak
-    terceInit();
-  radio.startListening();
-  Serial.write("\nSV1 LOW");
-  digitalWrite(signalLight1,LOW);
-  digitalWrite(signalLight2,LOW);
-  digitalWrite(waterValve,LOW);
-}
+  bool initDone = false;
+  bool blinkState = false;
+  size_t initMode = 0;
+  unsigned long time = 0, prevBlinkTime = 0;
 
-void terceInitFail()
-{
+  prevBlinkTime = millis();
 
+  while(!initDone)
+ {
+    time = millis();
+    if((time-prevBlinkTime)>1000)
+    {
+      digitalWrite(signalLight1,!blinkState);
+      digitalWrite(signalLight2,!blinkState);
+      prevBlinkTime = millis();
+    }
+
+    switch (initMode)
+    {
+      case 0:
+        Serial.write("\nINIT");
+        digitalWrite(signalLight1,HIGH);
+        digitalWrite(signalLight2,HIGH);
+        digitalWrite(waterValve,HIGH);
+        radio.stopListening();
+        initMode = 1;
+      break;
+      case 1:
+        if(radio.write(&text5, sizeof(text5))) // pokud se zprava neodesle opakujeme vysilani
+          initMode = 2;
+      break;
+
+      case 2:
+        radio.stopListening();
+        if(radio.available())
+          radio.read(&text,sizeof(text));
+        if(text == text5)                      // Pokud prijde echo, pokracujeme dal
+          initMode = 3;
+      break;
+
+      case 3:
+        digitalWrite(signalLight1,LOW);
+        digitalWrite(signalLight2,LOW);
+        digitalWrite(waterValve,LOW);
+        initDone = true;
+        initMode = 0;
+      break;
+    }
+  }
 }
 
